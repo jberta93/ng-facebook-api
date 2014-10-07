f741a1862e259fe2288504aef8654a2001bb2b6a
jberta93 <abonimo@gmail.com>
2014-10-08 00:58:20 +0200

    First pages commit

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -2,6 +2,6 @@
 
 angular
   .module('exampleApp',['ng-facebook-api']).config(function( facebookProvider) {
-	  facebookProvider.setInitParams('1529491150629819',true,true,true,'v2.1');
+	  facebookProvider.setInitParams('650566625058432',true,true,true,'v2.1');
 	  facebookProvider.setPermissions(['email','read_stream']);
   });
